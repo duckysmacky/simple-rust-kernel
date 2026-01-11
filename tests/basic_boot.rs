04dc87d2283a97823f3ed98c6f1cb7714f29f484
nikdor <kriperoka@gmail.com>
2026-01-11 16:09:28 +0300

    Add integration tests

---
+++ tests/basic_boot.rs
@@ -0,0 +1,24 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(kernel::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use kernel::println;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    kernel::test_panic_handler(info);
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    test_main();
+    loop {}
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
---
+++ tests/should_panic.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+use kernel::qemu::{self, QemuExitCode};
+use kernel::{serial_println, serial_print};
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    serial_println!("[ok]");
+    qemu::exit(QemuExitCode::Success);
+    loop {}
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    test_should_fail();
+    serial_println!("[test did not panic]");
+    qemu::exit(QemuExitCode::Failed);
+    loop {}
+}
+
+fn test_should_fail() {
+    serial_print!("should_panic::should_fail...\t");
+    assert_eq!(0, 1);
+}
