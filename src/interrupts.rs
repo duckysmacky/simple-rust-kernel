f282303660c60b4c7ea878b6828bdfe3d89af377
nikdor <kriperoka@gmail.com>
2026-01-16 21:45:53 +0300

    Add support for keyboard input

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,6 +1,7 @@
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use spin::Mutex;
 use pic8259::ChainedPics;
+use lazy_static::lazy_static;
 use crate::{gdt, print, println};
 
 pub const PIC_1_OFFSET: u8 = 32;
@@ -8,17 +9,19 @@ pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
 
 static PICS: Mutex<ChainedPics> = Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
 
-lazy_static::lazy_static! {
+lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut table = InterruptDescriptorTable::new();
 
-        table.breakpoint.set_handler_fn(breakpoint_handler);
+        table.breakpoint.set_handler_fn(handle_breakpoint_exception);
         unsafe {
             table.double_fault
-                .set_handler_fn(double_fault_handler)
+                .set_handler_fn(handle_double_fault_exception)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
-        table[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_handler);
+
+        table[InterruptIndex::Timer.as_usize()].set_handler_fn(handle_timer_interrupt);
+        table[InterruptIndex::Keyboard.as_usize()].set_handler_fn(handle_keyboard_interrupt);
 
         table
     };
@@ -28,6 +31,7 @@ lazy_static::lazy_static! {
 #[repr(u8)]
 pub enum InterruptIndex {
     Timer = PIC_1_OFFSET,
+    Keyboard,
 }
 
 impl InterruptIndex {
@@ -48,19 +52,51 @@ pub fn init_pics() {
     unsafe { PICS.lock().initialize() };
 }
 
-extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
+extern "x86-interrupt" fn handle_breakpoint_exception(stack_frame: InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
 
-extern "x86-interrupt" fn double_fault_handler(stack_frame: InterruptStackFrame, _error_code: u64) -> ! {
+extern "x86-interrupt" fn handle_double_fault_exception(stack_frame: InterruptStackFrame, _error_code: u64) -> ! {
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
 
-extern "x86-interrupt" fn timer_handler(_stack_frame: InterruptStackFrame) {
+extern "x86-interrupt" fn handle_timer_interrupt(_stack_frame: InterruptStackFrame) {
     print!(".");
 
     unsafe {
         PICS.lock().notify_end_of_interrupt(InterruptIndex::Timer.as_u8())
+    };
+}
+
+extern "x86-interrupt" fn handle_keyboard_interrupt(_stack_frame: InterruptStackFrame) {
+    use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
+    use spin::Mutex;
+    use x86_64::instructions::port::Port;
+
+    lazy_static! {
+        static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
+            Mutex::new(Keyboard::new(ScancodeSet1::new(), layouts::Us104Key, HandleControl::Ignore));
+    }
+
+    let mut port = Port::new(0x60);
+    let mut keyboard = KEYBOARD.lock();
+    let scancode: u8 = unsafe { port.read() };
+
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => {
+                    print!("{}", character);
+                }
+                DecodedKey::RawKey(key) => {
+                    print!("{:?}", key);
+                }
+            }
+        }
+    }
+
+    unsafe {
+        PICS.lock().notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8())
     };
 }
 
