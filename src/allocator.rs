564e72ff3f19f585bd2085240d1807c4dd0ed9c2
nikdor <kriperoka@gmail.com>
2026-01-24 19:12:34 +0300

    Add linked list allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,17 +1,17 @@
 pub mod bump;
+pub mod linked_list;
 
 use x86_64::structures::paging::mapper::MapToError;
 use x86_64::structures::paging::{FrameAllocator, Mapper, Size4KiB};
 use x86_64::VirtAddr;
 use spin::{Mutex, MutexGuard};
-
-use crate::allocator::bump::BumpAllocator;
+use crate::allocator::linked_list::LinkedListAllocator;
 
 pub const HEAP_START: usize = 0x4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024;
 
 #[global_allocator]
-static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
+static ALLOCATOR: Locked<LinkedListAllocator> = Locked::new(LinkedListAllocator::new());
 
 struct Locked<A> {
     inner: Mutex<A>,
---
+++ src/allocator/linked_list.rs
@@ -0,0 +1,152 @@
+use core::mem;
+use alloc::alloc::{GlobalAlloc, Layout};
+use super::Locked;
+
+struct RegionNode {
+    size: usize,
+    next: Option<&'static mut RegionNode>,
+}
+
+impl RegionNode {
+    const fn new(size: usize) -> Self {
+        RegionNode { size, next: None }
+    }
+
+    fn start_addr(&self) -> usize {
+        self as *const Self as usize
+    }
+
+    fn end_addr(&self) -> usize {
+        self.start_addr() + self.size
+    }
+}
+
+pub struct LinkedListAllocator {
+    head: RegionNode,
+}
+
+impl LinkedListAllocator {
+    /// Creates an empty LinkedListAllocator.
+    pub const fn new() -> Self {
+        Self {
+            head: RegionNode::new(0),
+        }
+    }
+
+    /// Initialize the allocator with the given heap bounds.
+    ///
+    /// This function is unsafe because the caller must guarantee that the given
+    /// heap bounds are valid and that the heap is unused. This method must be
+    /// called only once.
+    pub unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
+        unsafe {
+            self.add_free_region(heap_start, heap_size);
+        }
+    }
+
+    /// Adds the given memory region to the front of the list.
+    unsafe fn add_free_region(&mut self, addr: usize, size: usize) {
+        assert_eq!(super::align_up(addr, mem::align_of::<RegionNode>()), addr);
+        assert!(size >= mem::size_of::<RegionNode>());
+
+        // TODO: insert in an ordered manner
+
+        let mut node = RegionNode::new(size);
+        node.next = self.head.next.take();
+
+        let node_ptr = addr as *mut RegionNode;
+        unsafe {
+            node_ptr.write(node);
+            self.head.next = Some(&mut *node_ptr)
+        }
+    }
+
+    /// Looks for a free region with the given size and alignment and removes
+    /// it from the list.
+    ///
+    /// Returns a tuple of the list node and the start address of the allocation.
+    fn find_region(&mut self, size: usize, align: usize) -> Option<(&'static mut RegionNode, usize)> {
+        let mut current = &mut self.head;
+
+        while let Some(ref mut region) = current.next {
+            if let Ok(alloc_start) = Self::alloc_from_region(&region, size, align) {
+                let next = region.next.take();
+                let ret = Some((current.next.take().unwrap(), alloc_start));
+                current.next = next;
+                return ret;
+            } else {
+                current = current.next.as_mut().unwrap();
+            }
+        }
+
+        None
+    }
+
+    /// Try to use the given region for an allocation with given size and
+    /// alignment.
+    ///
+    /// Returns the allocation start address on success.
+    fn alloc_from_region(region: &RegionNode, size: usize, align: usize) -> Result<usize, ()> {
+        let alloc_start = super::align_up(region.start_addr(), align);
+        let alloc_end = alloc_start.checked_add(size).ok_or(())?;
+
+        if alloc_end > region.end_addr() {
+            return Err(());
+        }
+
+        let excess_size = region.end_addr() - alloc_end;
+        if excess_size > 0 && excess_size < mem::size_of::<RegionNode>() {
+            return Err(());
+        }
+
+        Ok(alloc_start)
+    }
+
+    /// Adjust the given layout so that the resulting allocated memory
+    /// region is also capable of storing a `ListNode`.
+    ///
+    /// Returns the adjusted size and alignment as a (size, align) tuple.
+    fn size_align(layout: Layout) -> (usize, usize) {
+        let layout = layout
+            .align_to(mem::align_of::<RegionNode>())
+            .expect("Adjusting alignment failed")
+            .pad_to_align();
+
+        let size = layout.size().max(mem::size_of::<RegionNode>());
+
+        (size, layout.align())
+    }
+}
+
+unsafe impl GlobalAlloc for Locked<LinkedListAllocator> {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        use core::ptr;
+
+        let (size, align) = LinkedListAllocator::size_align(layout);
+        let mut allocator = self.lock();
+
+        if let Some((region, alloc_start)) = allocator.find_region(size, align) {
+            let alloc_end = alloc_start.checked_add(size).expect("Overflow");
+            let excess_size = region.end_addr() - alloc_end;
+
+            if excess_size > 0 {
+                unsafe {
+                    allocator.add_free_region(alloc_end, excess_size);
+                }
+            }
+
+            alloc_start as *mut u8
+        } else {
+            ptr::null_mut()
+        }
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        let (size, _) = LinkedListAllocator::size_align(layout);
+        let mut allocator = self.lock();
+
+        unsafe {
+            allocator.add_free_region(ptr as usize, size);
+        }
+    }
+}
